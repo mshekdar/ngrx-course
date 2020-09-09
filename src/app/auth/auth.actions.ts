7fcfc4ffe3d92b7373ace45d44de20098e86afae
Sonal <dongare.sonal@gmail.com>
2020-09-09 18:35:18 +0530

    created login reducer to update store state

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -1,7 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { User } from './model/user.model';
 
-export const login = createAction(
-  '[Login page] User Login',
-  props<{user: User}>()
-);
+export const login = createAction('[Login page] User Login', props<{user: User}>());
+
+export const logout = createAction('[Top Menu] User Logout');
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Store } from "@ngrx/store";
+import { tap } from "rxjs/operators";
+import { Router } from "@angular/router";
+import { noop } from "rxjs";
 
-import {Store} from "@ngrx/store";
-
-import {AuthService} from "../auth.service";
-import {tap} from "rxjs/operators";
-import {noop} from "rxjs";
-import {Router} from "@angular/router";
+import { AuthService } from "../auth.service";
 import { AppState } from 'app/reducers';
-import { login } from "../auth.actions";
+import { AuthActions } from '../action-types';
+
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
@@ -19,15 +19,15 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
 
   constructor(
-      private fb:FormBuilder,
-      private auth: AuthService,
-      private router:Router,
-      private store: Store<AppState>) {
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+    private store: Store<AppState>) {
 
-      this.form = fb.group({
-          email: ['test@angular-university.io', [Validators.required]],
-          password: ['test', [Validators.required]]
-      });
+    this.form = fb.group({
+      email: ['test@angular-university.io', [Validators.required]],
+      password: ['test', [Validators.required]]
+    });
 
   }
 
@@ -41,7 +41,7 @@ export class LoginComponent implements OnInit {
       .pipe(
         tap(user => {
           console.log(user);
-          this.store.dispatch(login({user}))
+          this.store.dispatch(AuthActions.login({ user }))
           this.router.navigateByUrl('/courses')
         })
       )
