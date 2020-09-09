e8b915840699fe4736908edb6c7f3a1bf130b215
Sonal <dongare.sonal@gmail.com>
2020-09-09 17:51:13 +0530

    added login action

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,7 +7,8 @@ import {AuthService} from "../auth.service";
 import {tap} from "rxjs/operators";
 import {noop} from "rxjs";
 import {Router} from "@angular/router";
-
+import { AppState } from 'app/reducers';
+import { login } from "../auth.actions";
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
@@ -20,7 +21,8 @@ export class LoginComponent implements OnInit {
   constructor(
       private fb:FormBuilder,
       private auth: AuthService,
-      private router:Router) {
+      private router:Router,
+      private store: Store<AppState>) {
 
       this.form = fb.group({
           email: ['test@angular-university.io', [Validators.required]],
@@ -34,7 +36,19 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-
+    const loginInfo = this.form.value;
+    this.auth.login(loginInfo.email, loginInfo.password)
+      .pipe(
+        tap(user => {
+          console.log(user);
+          this.store.dispatch(login({user}))
+          this.router.navigateByUrl('/courses')
+        })
+      )
+      .subscribe(
+        noop,
+        () => alert('Login failed!')
+      )
   }
 
 }
