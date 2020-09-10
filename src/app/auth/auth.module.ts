796333b846bab0a214e61993ff80068df36533fa
Mayuresh <shekdarm@gmail.com>
2020-09-10 11:34:11 +0530

    basic setup for ngrx effects

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,17 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
 import { MatCardModule } from "@angular/material/card";
 import { MatInputModule } from "@angular/material/input";
 import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { LoginComponent } from './login/login.component';
 import { AuthService } from "./auth.service";
 import { AuthGuard } from "./auth.guard";
-import { EffectsModule } from '@ngrx/effects';
-import * as fromAuth from './reducers';
+import { AuthEffects } from "./auth.effects";
 import { authReducer } from './reducers';
 
 @NgModule({
@@ -22,6 +23,7 @@ import { authReducer } from './reducers';
     MatButtonModule,
     RouterModule.forChild([{ path: '', component: LoginComponent }]),
     StoreModule.forFeature('auth', authReducer),
+    EffectsModule.forFeature([AuthEffects]),
   ],
   declarations: [LoginComponent],
   exports: [LoginComponent]
