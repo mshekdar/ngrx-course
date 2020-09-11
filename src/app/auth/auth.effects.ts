f9ad1538d48a1d4bb4a99c426be51f3fa4f7b5f3
Mayuresh <shekdarm@gmail.com>
2020-09-11 09:58:27 +0530

    auth effect with createEffect

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,15 +1,23 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { tap } from "rxjs/operators";
 import { AuthActions } from "./action-types";
 
 @Injectable()
 export class AuthEffects {
-  constructor(private actions$: Actions) {
-    actions$.subscribe(action => {
-      if (action.type === '[Login page] User Login') {
-        localStorage.setItem('user', JSON.stringify(action['user']));
-      }
-    })
-  }
+
+  /**
+   * Login effect to save user to localstorage
+   */
+  login$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(AuthActions.login),
+      tap((action) => localStorage.setItem('user', JSON.stringify(action.user)))
+    )},
+    // No further action dispatch
+    { dispatch: false }
+  );
+
+  constructor(private actions$: Actions) { }
 
 }
